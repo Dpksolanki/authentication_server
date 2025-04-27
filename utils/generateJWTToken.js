33660c0dba3d42438e394c627267497056254cc8
Dpksolanki <93882560+Dpksolanki@users.noreply.github.com>
2025-04-27 09:00:08 +0530

    Update generateJWTToken.js

--- utils/generateJWTToken.js
+++ utils/generateJWTToken.js
@@ -14,7 +14,7 @@ export const generateJWTToken = (res, userId) => {
     // Set the token as a cookie in the response
     res.cookie('token', token, {
         httpOnly: true, // Prevent client-side access to the cookie
-        secure: process.env.NODE_ENV === 'local', // Use secure cookies in production
+        secure: process.env.NODE_ENV === 'local', 
         sameSite: 'strict', // Prevent CSRF attacks
         maxAge: 7 * 24 * 60 * 60 * 1000 // Cookie expiration time
     });
